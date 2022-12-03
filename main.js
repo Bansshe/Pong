45688a0db4fc696b5beace5acfce31bd48c96103
Douglas Silva <79361516+Bansshe@users.noreply.github.com>
2022-12-03 12:50:33 -0300

    restructuring

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-let xBola = 300;
-let yBola = 200;
+let xBola = 15;
+let yBola = 15;
 let dBola = 20;
+let raio = dBola / 2;
 
 let velXBola = 1;
 let velYBola = 1;
 
 function setup() {
-  createCanvas(600, 400);
+    createCanvas(600, 400);
+    background(30);
 }
-  
+
+function criaBola() {
+    circle(xBola, yBola, dBola);
+}
+
 function draw() {
-  background(30);
-  circle(xBola, yBola, dBola);
-  xBola += velXBola;
-  yBola += velYBola;
-  
-  if (xBola >= width-10 || xBola <= 10) {  
-    velXBola *= -1;
-  }
-  
-  if (yBola >= height-10 || yBola <= 10) {  
-    velYBola *= -1;
-  }
+    background(30);
+    criaBola();
+    verificaColisao();
+    xBola += velXBola;
+    yBola += velYBola;
+
+}
+
+function verificaColisao() {
+    if (xBola >= width - 10 || xBola <= raio) {
+        velXBola *= -1;
+    }
+
+    if (yBola >= height - 10 || yBola <= raio) {
+        velYBola *= -1;
+    }
+
 }
