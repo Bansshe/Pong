565ee74d8dde7ed83b610db2d54411d5a80111de
Douglas Silva <79361516+Bansshe@users.noreply.github.com>
2022-12-03 22:49:09 -0300

    adding functions

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-let xBola = 15;
+var xBola = 15;
 let yBola = 15;
 let dBola = 20;
 let raio = dBola / 2;
+let xRaquete = 5;
+let yRaquete = 150;
+let comprimentoRaquete = 10;
+let alturaRaquete = 190;
+let bordaRaquete = 0;
 
-let velXBola = 1;
-let velYBola = 1;
+let velXBola = 10;
+let velYBola = 5;
 
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(1900, 900);
     background(30);
 }
 
@@ -19,6 +24,10 @@ function draw() {
     background(30);
     criaBola();
     verificaColisao();
+    criaRaquete();
+    movimentarRaquete();
+    colisaoRaquete();
+    
     xBola += velXBola;
     yBola += velYBola;
 
@@ -32,5 +41,23 @@ function verificaColisao() {
     if (yBola >= height - 10 || yBola <= raio) {
         velYBola *= -1;
     }
-
 }
+
+function criaRaquete() {
+    rect(xRaquete, yRaquete, comprimentoRaquete, alturaRaquete, bordaRaquete);
+}
+
+function movimentarRaquete() {
+    if (keyIsDown(UP_ARROW)){
+        yRaquete -= 9;
+    }
+    if (keyIsDown(DOWN_ARROW)){
+        yRaquete += 9;
+    }
+}
+
+function colisaoRaquete(){
+    if (xBola - raio < xRaquete + comprimentoRaquete && yBola - raio < yRaquete + alturaRaquete && yBola + raio > yRaquete){
+      velXBola *= -1;
+    }
+  }
